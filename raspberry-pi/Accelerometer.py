cb2c1d36bab9ed2a099ed6790a4ce9ba25b0be01
inovotn04 <inovotn04@charlottesvilleschools.org>
2022-09-16 10:43:53 -0400

    PAIN

--- raspberry-pi/Accelerometer.py
+++ raspberry-pi/Accelerometer.py
@@ -1,8 +1,18 @@
 import board
 import time
 import digitalio
+import adafruit_mpu6050
+import busio
 
-led_blue = digitalio.DigitalInOut(board.GP13)
-led_blue.direction = digitalio.Direction.OUTPUT
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(scl_pin, sda_pin)
 
-led_blue.value = False
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True:
+    x = mpu.acceleration[0]
+    y = mpu.acceleration[1]
+    z = mpu.acceleration[2] #Pulls all the mpu acceleration values
+
+    print(f"x: {x}  y: {y}  z: {z}")
